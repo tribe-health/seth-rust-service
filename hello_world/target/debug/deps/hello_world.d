7fb4bb0482fe76b425224de54a94f743c7e09260
Sauz21 <87775470+Sauz21@users.noreply.github.com>
2021-07-21 17:19:25 -0500

    Add files via upload

---
+++ hello_world/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+C:\Users\16019\Desktop\rust\hello_world\target\debug\deps\hello_world.exe: src\main.rs
+
+C:\Users\16019\Desktop\rust\hello_world\target\debug\deps\hello_world.d: src\main.rs
+
+src\main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+C:\Users\16019\Desktop\rust\hello_world\target\debug\hello_world.exe: C:\Users\16019\Desktop\rust\hello_world\src\main.rs
